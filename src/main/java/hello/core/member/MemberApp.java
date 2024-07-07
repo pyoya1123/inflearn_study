9a962e8f817848e2e252af67ea6d0861f1fbd468
pyoya1123 <pyoya1123@gmail.com>
2024-07-08 03:27:26 +0900

    ioc_di_and_container

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -10,6 +10,14 @@ public class MemberApp {
     이젠 AppConfig를 이용해서 개발할거임.
      */
 
+    /*
+    이렇게 MemberApp같은 경우는 개발자가 직접 제어함. 뭐 new해가지고 돌리고
+    이런거는 개발자가 직접 순차적으로 돌리는거임. 반면에 내가 작성한 코드가 직접 제어의 흐름을 담당한다면
+    그것은 프레임워크가 아니라 라이브러리다.
+    예를 들어서 우리가 뭐 자바 객체를 xml로 바꾸거나, json으로 바꾸는게 있음.
+    그럼 내가 그 라이브러리를 불러다가 직접 호출함. 이런거는 라이브러리라고 보면 된다.
+     */
+
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         // 밑에 memberService에는 멤버서비스impl이 들어가있을거임.
