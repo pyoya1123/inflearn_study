78d1e09108b935bf72dd35951357f2ed1ddb1598
pyoya1123 <pyoya1123@gmail.com>
2024-07-11 02:00:23 +0900

    다양한 의존관계 주입 방법(필드 주입)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,56 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
     /*
-    지금 내용은 수정 주입 내용임.
-    필드를 수정하려면 관례상 set 함수를 사용함. 자바 빈 프로퍼티 규약임.
-    OrderServiceImpl을 스프링 컨테이너에 딱 등록을 함.
-    그다음에 어떻게 하냐?
-    스프링 컨테이너는 크게 두가지 라이프사이클이 있음.
-    첫번쨰는 뭐냐면, 스프링 빈을 등록하는거임. 스프링 빈을 다 등록하는거임.
-    그다음에 뭘하냐? 연관 관계를 자동으로 주입한다.
-    바로 이 Autowired 어노테이션이 붙은 애들을 자동으로 주입함.
-    처음에 스프링이 빈을 생성하는 단계랑 의존관계 주입하는 단계가 나눠져있다고 했었음.
+    안티패턴임.
+    외부에서 변경이 안된다.
+    예를 들어, 더미 데이터로 임시적으로 테스트를 해보려고 위에 객체들에 값을 대입하려고 하지만,
+    순수 자바 코드로는 값을 변경할 수 있는 방법이 존재하지않음.
+    OrderServiceTest.java 코드 에서 추가 설명함.
      */
-
-
-    /*
-    생성자가 하나 일 땐 Autowired를 생략해도 알아서 해줌.
-    생성자가 두개 일 땐 Autowired 지정하기.
-     */
-
-    /*
-    생성자 주입은, 일단 이 파일은 자바 코드임. 스프링이 뭐 용뺴는 재주가 있는건 아님.
-    그래서 OrderServiceImpl 객체를 스프링이 생성해서 빈에 등록을 해야함.
-    그럼 뭘 호출해야하냐? 스프링도 결국 new OrderServiceImpl 이렇게 해서 호출해야함.
-    이러면 결국 생성자가 필요한거임. 그럼 스프링도 결국 스프링 컨테이너에서 밑에 memberRepository와 discountPolicy 두 개, 아 이거
-    Autowired네 하고 스프링 컨테이너에서 스프링 빈 등록된거 찾아와가지고 new OrderServiceImpl(memberRepository, discountPolicy)
-    이렇게해서 얘를 생성함.
-    그래서 생성자는 어쩔 수 없이 스프링 라이프사이클 그 빈 등록할 때 자동 주입이 일어나는거임.
-    그리고 수정자 주입은 의존관계 주입, 2번째 단게에서 일어나는거임.
-    순서를 보면, 생성자 먼저 호출되고, 위에 set 함수들 차레로 호출되는거임.
-
-    결과적으로 밑에 생성자 호출하는 코드는 필요가 없어지긴함. 위에 set함수들로 값 대입하니까.
-     */
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -35,4 +35,20 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+    // 추가 설명
+    /*
+    밑에 코드처럼 구현하고 실행하면 NullPointerException이 발생할거임. OrderServiceImpl 파일에서 createOrder 함수에서 발생함.
+    왜냐하면 new해서 객체 생성하고 실행하는건 스프링에서 돌리는게 아님.
+    그냥 순수하게 자바로 돌리는거임.
+    결국 memberRepository와 discountPolity에 값을 대입하는 set함수를 각각 구현해줘야하는거임.
+    이래서 필드 인젝션은 안씀.
+    Autowired는 스프링 컨테이너가 관리하는걸 가져와야 적용이 되는거지, 내가 임의로 new해서 생성하는 애는
+    당연히 Autowired가 적용이 안됨.
+     */
+    @Test
+    void fieldInjectionTest() {
+        OrderServiceImpl orderService = new OrderServiceImpl();
+        orderService.createOrder(1L, "itemA", 10000);
+    }
 }
