8dabdbea0e0cbe18dbb94bd52ef2b78f80d15077
jeonseongpyo <pyoya1123@gmail.com>
2024-07-13 10:54:42 +0900

    애노테이션 @PostContruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -30,14 +32,14 @@ public class NetworkClient{
     }
 
 
-
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
