d4213f542417e6a3f474fd817200857aa5e07da6
pyoya1123 <pyoya1123@gmail.com>
2024-07-15 02:42:41 +0900

    스프링 의존 provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -35,19 +36,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            /*
+            getObject를 호출하면 얘가 그때서야 스프링 컨테이너에서 프로토타입 빈을 찾아서 반환해주는거임.
+            우리가 ApplicationContext한테 직접 찾는게 아니라, provider가 찾아주는 기능만 제공하는거임.
+            그러면 ApplicationContext에는 많은 기능들이 있는데 이것들을 다 보는게 아니라, 딱 필요한 부분만 찾아서 쓰는 느낌임.
+            그리고 우리가 원했던 필요할 때마다 스프링 컨테이너에 요청하는 기능을 사용할 수 있는거임.
+             */
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
